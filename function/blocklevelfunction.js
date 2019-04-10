a5c4efba110afbaa8860a7a25e5659cfd0fa7262
ndgspn <nandangsopyan08@gmail.com>
2019-04-10 14:03:28 +0700

    add blocklevelfunction.js

---
+++ function/blocklevelfunction.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function f(){
+  return 1;
+}
+
+{
+  function f() {
+    return 2;
+  }
+}
+
+console.log(f() === 1);
--- function/threeoffunction.js
+++ function/threeoffunction.js
@@ -1,12 +1,12 @@
 var expr = function() {
   console.log("Ini contoh function expression di js");
-}
+};
 
 function decl(){
   console.log("Ini contoh function declaration di js");
 }
 
-var cons = new Function('console.log("Ini contoh function constructor di js")');
+var cons = new Function('console.log("Ini contoh function constructor di js");');
 
 expr();
 decl();
